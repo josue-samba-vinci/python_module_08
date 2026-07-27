f6b8a0b6ca406d83ee0f9186f3103126a3ff0909
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-07-27 16:44:37 +0200

    main in ex1 and check of edge cases

--- ex1/loading.py
+++ ex1/loading.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 try:
     import numpy
 except ImportError:
@@ -26,13 +27,15 @@ def check_dependencies() -> bool:
     else:
         print(f"[OK] pandas ({pandas.__version__}) - Data manipulation ready")
     if numpy is None:
-        print("[KO] numpy - Numerical computing not installed")
+        print("[KO] numpy - Numerical computation not installed")
     else:
-        print(f"[OK] numpy ({numpy.__version__}) - Numerical computing ready")
+        print("[OK] numpy "
+              f"({numpy.__version__}) - Numerical computation ready")
     if plt is None:
         print("[KO] matplotlib - Visualization not installed")
     else:
-        print(f"[OK] matplotlib ({matplotlib.__version__}) - Visualization ready")
+        print("[OK] matplotlib ("
+              f"{matplotlib.__version__}) - Visualization ready")
     return all([pandas, numpy, matplotlib])
 
 
@@ -42,8 +45,42 @@ def generate_matrix_data(size: int) -> numpy.ndarray:
         raise ImportError("numpy is required to generate matrix data.")
     return numpy.random.rand(size)
 
-def process_data(data: numpy.ndarray) -> pandas.DataFrame:
+
+def process_matrix_data(data: numpy.ndarray) -> pandas.DataFrame:
     """Process the matrix data and return a DataFrame."""
     if pandas is None:
         raise ImportError("pandas is required to process data.")
-    pandas.
+    dataframe = pandas.DataFrame(data, columns=["numbers"])
+    return dataframe
+
+
+def display_matrix(dataframe: pandas.DataFrame) -> None:
+    if plt is None:
+        raise ImportError("pyplot is required to display the matrix")
+    plt.hist(dataframe)
+    plt.savefig("matrix_analysis.png")
+
+
+def main() -> None:
+    print()
+    print("LOADING STATUS: Loading programs...")
+    print()
+    boolean = check_dependencies()
+    print()
+    if boolean:
+        data = generate_matrix_data(1000)
+        print("Analyzing Matrix data...")
+        print("Processing 1000 data points...")
+        dataframe = process_matrix_data(data)
+        print("Generating visualization...")
+        display_matrix(dataframe)
+        print()
+        print("Analysis complete!")
+        print("Results saved to: matrix_analysis.png")
+    else:
+        print("Please install all the required dependencies using :")
+        print("<pip install -r requirements.txt> or <poetry install>")
+
+
+if __name__ == "__main__":
+    main()
